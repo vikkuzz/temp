e665dc939ae5bf232d7549f85aa39062d4bd9b98
Vikkuzz <simpat007@yandex.ru>
2025-02-08 16:40:15 +0300

    feat: finish course

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  // output: 'export',
+  experimental: {
+    ppr: 'incremental'
+  }
   /* config options here */
 };
 
